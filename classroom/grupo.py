2c6ef283f31e356047ffef0c4c0a91d7a1771758
Melissa-mon <71413547+Melissa-mon@users.noreply.github.com>
2024-11-25 21:57:02 +0000

    jejeje

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,70 +1,26 @@
 from classroom.asignatura import Asignatura
 
-class Asignatura:
-
-    def __init__(self, nombre, salon):
-        self._nombre = nombre
-        self._salon = salon
-
-    def __str__(self):
-        return f"{self._nombre} {self._salon}"
-
-
 class Grupo:
-    grado = None
+    grado = "Grado 12"  # Valor por defecto
 
-    def __init__(self, estudiantes, grupo="grupo ordinado", asignaturas=None):
+    def _init_(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas if asignaturas is not None else []
-        self.listadoAlumnos = estudiantes
+        self.listadoAlumnos = estudiantes if estudiantes is not None else []
 
-    def listadoAsignaturas(self, kwargs):
-        for nombre, salon in kwargs.items():
-            self._asignaturas.append(Asignatura(nombre, salon))
+    def listadoAsignaturas(self, **kwargs):
+        for x in kwargs.values():
+            self._asignaturas.append(Asignatura(x))
 
     def agregarAlumno(self, alumno, lista=None):
         if lista is None:
             lista = []
         lista.append(alumno)
-        self.listadoAlumnos.extend(lista)
+        self.listadoAlumnos += lista
 
-    def __str__(self):
+    def _str_(self):
         return f"Grupo de estudiantes: {self._grupo}"
 
     @classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
+    def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
-
-
-if __name__ == "__main__":
-    # Crear una asignatura e imprimirla
-    Asignatura1 = Asignatura("Matematicas", "remoto")
-    print(Asignatura1)  # Matematicas remoto
-
-    # Crear un grupo de estudiantes e imprimirlo
-    grupo = Grupo(estudiantes=[], grupo="grupo predeterminado")
-    print(grupo)  # Grupo de estudiantes: grupo predeterminado
-
-    # Asignar nombre de grado y mostrarlo
-    Grupo.asignarNombre("Grado 12")
-    print(Grupo.grado)  # Grado 12
-
-    # Agregar estudiantes al grupo e imprimir el listado
-    grupo.agregarAlumno("Kelly")
-    print(grupo.listadoAlumnos)  # ['Kelly']
-
-    grupo.agregarAlumno("Jaime", ["Pedro", "Santiago"])
-    print(grupo.listadoAlumnos)  # ['Jaime', 'Pedro', 'Santiago']
-
-    grupo.agregarAlumno("Javier")
-    print(grupo.listadoAlumnos)  # ['Jaime', 'Pedro', 'Santiago', 'Javier']
-
-    # Imprimir el número de estudiantes
-    print(len(grupo.listadoAlumnos))  # 4
-
-    # Asignar un nuevo grado y mostrarlo
-    Grupo.asignarNombre("Grado 1")
-    print(Grupo.grado)  # Grado 1
-
-    Grupo.asignarNombre("Grado 6")
-    print(Grupo.grado)  # Grado 6
